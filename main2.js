c7ed40f57408e15fb76cd8aeb6cf6f6b113e9883
Trung <86226130+TrungHere@users.noreply.github.com>
2022-06-18 09:41:12 +0700

    learn2 18/6

--- main2.js
+++ main2.js
@@ -1,7 +1,7 @@
 
-
+console.log('========Cach1=========')
 var agejohn = 0;
-
+var agejohn1 = 0;
 //hàm - trả về return
 function tinhtuoi(year) {
     //tinh toan
@@ -17,3 +17,18 @@ thaydoituoi(); //loi goi ham`
 var age = tinhtuoi(agejohn);
 
 console.log('tuoi cua John la : ',age);
+
+console.log('==========Cach2===========');
+
+var tinhtuoi1 = (year) => {
+    //to do sth...
+    return 2022 - year; 
+}
+
+var thaydoituoi = () => {
+    agejohn1 = 1998;
+}
+
+thaydoituoi(); //loi goi ham
+var age = tinhtuoi1(agejohn1);
+console.log('tuoi cua John la :',age);

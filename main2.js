aaf3d81a929aadc6789ac3fb0e19204cf5462a14
Trung <86226130+TrungHere@users.noreply.github.com>
2022-06-20 17:04:28 +0700

    function - 20/6

--- main2.js
+++ main2.js
@@ -2,6 +2,8 @@
 console.log('========Cach1=========')
 var agejohn = 0;
 var agejohn1 = 0;
+var name;
+var job;
 //hàm - trả về return
 function tinhtuoi(year) {
     //tinh toan
@@ -32,3 +34,36 @@ var thaydoituoi = () => {
 thaydoituoi(); //loi goi ham
 var age = tinhtuoi1(agejohn1);
 console.log('tuoi cua John la :',age);
+
+console.log('==============================');
+//var name, job;
+//job = teacher
+//name = Trung
+var printcontent = (name, job) => {
+    switch (job) {
+        case 'designer':
+            console.log(name + ' la 1 nha thiet ke');
+            
+            break;
+        case 'doctor':
+            console.log(name + ' la 1 bac si');
+
+            break;
+        case 'teacher':
+            console.log(name + ' la 1 giao vien');
+
+            break;
+        default:
+            console.log(name + ' la 1 nguoi khong thanh cong');
+            break;
+    }
+    
+}
+
+printcontent('Trung','designer');
+printcontent('Trung','doctor');
+printcontent('Trung','teacher');
+printcontent('Trung','')
+
+console.log('==============================')
+

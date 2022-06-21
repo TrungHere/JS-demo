4f59e0b7eb2e225493629e43070e117479a700c3
Trunghere <trunghere@gmail.com>
2022-06-21 14:51:15 +0700

    dell 5285 21/6(1)

--- main3.js
+++ main3.js
@@ -4,3 +4,20 @@ var arrStudents = [
     'Nguyen Van C'
 ];
 console.log('arrStudents :', arrStudents);
+console.log('arrStudents length :', arrStudents.length);
+console.log('arrStudents[0] :', arrStudents[0]);
+console.log('arrStudents[lastest]  :', arrStudents[arrStudents.length - 1]);
+
+var arrStudents1 = new Array('NV A','NV B', 'NV C');
+console.log('arrStudent1', arrStudents1);
+
+var arrDemo = [
+    'Nguyen Hoang Lam',
+    28,
+    true,
+    [01,11,10]
+
+]
+console.log('arrDemo = ', arrDemo);
+console.log('arrDemo = ', arrDemo[3][2]);
+console.log('arrDemo = ', arrDemo[2]);
